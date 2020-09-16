c1ccf0a0bdad1e2058e48e985331c23c64152ec4
Jazmin CamG <jazmin.camacho7450@alumnos.udg.mx>
2020-09-16 00:17:48 -0500

    Capturamos nombre completo

--- main.c
+++ main.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
 int main(){
+    char nombre[100];
     printf("Hola mundo\n");
+    printf("Nombre: ");
+    scanf("%s", nombre);
+
+    printf("Hola! %s", nombre);
 
     return 0;
 
